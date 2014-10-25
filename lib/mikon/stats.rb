511d47f2e5fa28e638fa4a582aafa763556ca0f2
domitry <domitry@gmail.com>
2014-10-25 22:34:02 +0900

    Add the module 'Stats'

---
+++ lib/mikon/stats.rb
@@ -0,0 +1,54 @@
+module Mikon
+  module Stats
+    extend Forwardable
+    def_delegators @data, :size, :*, :+, :-, :==, :[], :[]=, :max, :min, :push
+    alias_method :size, :n
+    alias_method :standard_deviation_sample, :sd
+    alias_method :standard_deviation_sample, :sds
+    alias_method :standard_deviation_population, :sdp
+    alias_method :standard_error, :se
+
+    def median
+      self.percentil(50)
+    end
+
+    def mode
+    end
+
+    def n_valid
+    end
+
+    def percentil(p)
+    end
+
+    def product
+      @data.inject(1){|memo, val| memo*val}
+    end
+
+    def proportion
+    end
+
+    def proportion_confidence_interval_t
+      raise "NotImplementedError"
+    end
+
+    def proportion_confidence_interval_z
+      raise "NotImplementedError"
+    end
+
+    def proportions
+    end
+
+    def range
+      max - min
+    end
+
+    def mean
+      @data.mean.first
+    end
+  end
+
+  class DArray
+    include Stats
+  end
+end
